ed61217593136cd09c01513e0a1203fd64e4a4d4
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-15 14:11:15 -0400

    Fixed MultProgSyncMatMult.py

--- MatrixMult/MultProgSyncMatMult.py
+++ MatrixMult/MultProgSyncMatMult.py
@@ -3,21 +3,27 @@
 # Project Code: Python's Built in Multiprogramming Library Synchronous Matrix Multiplication
 
 import numpy as np
-import multiprocessing
+import multiprocessing as mp
+import ctypes as c
 import cProfile
+
+def CreateArray(m, n):
+    mp_array = mp.Array('d', m*n)
+    array = np.frombuffer(mp_array.get_obj(), c.c_int64)
+    b = array.reshape((m, n))
+
+    return b
 
 def matrixWork(offset, rows_to_send, matrix1, matrix2, final_matrix):
     for i in range(len(matrix1)):
-        for j in range(0, rows_to_send+1):
+        for j in range(offset, offset+rows_to_send):
             for k in range(len(matrix2)):
-                final_matrix[i][j] += matrix1[i][k] * matrix2[k][offset]
+                final_matrix[i*len(matrix1)+j] += matrix1[i][k] * matrix2[k][j]
 
-    return final_matrix
 
 def workMethod():
     matrix1 = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]])
     matrix2 = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45], [50, 55, 60], [65, 70, 75], [80, 85, 90]])
-    final_matrix = np.zeros((3, 3))
 
     # Expected Results:
     # [1155, 1260, 1365]
@@ -32,15 +38,31 @@ def workMethod():
     print(matrix2)
     print('\n')
 
-    number_processes = 6
+    number_processes = 3
 
-    if number_processes < 4:
-        print('Error: To little processes! Must be greater than 4')
+    if number_processes < 2:
+        print('Error: To little processes! Must be greater than 2')
         return
-    else:
-        print('Filler')
 
+    processes = []
 
+    with mp.Manager() as manager:
+        offset = 0
+        rows_to_send = 1
+        final_matrix = mp.Array('i', 3*3)
+        for _ in range(number_processes):
+            p = mp.Process(target=matrixWork, args=[offset, rows_to_send, matrix1, matrix2, final_matrix])
+            p.start()
+            processes.append(p)
+            offset += 1
+
+        for p in processes:
+            p.join()
+
+    array = np.frombuffer(final_matrix.get_obj(), c.c_int32)
+    k = array.reshape((3, 3))
+
+    print(k)
 
 def main():
     cProfile.run('workMethod()')
