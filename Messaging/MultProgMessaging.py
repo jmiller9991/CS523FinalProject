a907895fa88497cb5bb8264d67644d172276dc65
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-17 01:32:18 -0400

    Finished Multiprogramming Messages

---
+++ Messaging/MultProgMessaging.py
@@ -0,0 +1,29 @@
+# Jacob Miller
+# 4/12/2021
+# Project Code: Python's Built in Multiprogramming Library Messaging Function
+
+import multiprocessing as mp
+import cProfile
+import os
+
+def processMessage(parentProcess):
+    print(f'Hello {parentProcess}, this is {os.getpid()}.')
+
+def doWork():
+    processes = []
+
+    for _ in range(5):
+        p = mp.Process(target=processMessage, args=[os.getpid()])
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    print('Done')
+
+def main():
+    cProfile.run('doWork()')
+
+if __name__ == '__main__':
+    main()
