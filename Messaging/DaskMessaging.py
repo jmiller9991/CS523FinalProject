a410293f8ba41c088dc084296158a0cbf3c49684
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-19 00:54:49 -0400

    Doing Dask assignment and has problem with os.getpid printing same id for projects.

---
+++ Messaging/DaskMessaging.py
@@ -0,0 +1,31 @@
+# Jacob Miller
+# 4/19/2021
+# Project Code: Dask Library Exponential Function
+
+import dask as da
+import cProfile
+import os
+
+def processMessage(parentProcess):
+    print(f'Hello {parentProcess}, this is {os.getpid()}.')
+
+def doWork():
+    delayed_work = []
+    for _ in range(5):
+        p = da.delayed(processMessage)(os.getpid())
+        delayed_work.append(p)
+
+    num = 0
+    for i in delayed_work:
+        i.visualize(filename=f'./Results/DaskMessagingResults/DaskMessagingProcess{num}')
+        num += 1
+
+    for p in delayed_work:
+        p.compute()
+
+
+def main():
+    cProfile.run('doWork()')
+
+if __name__ == '__main__':
+    main()
