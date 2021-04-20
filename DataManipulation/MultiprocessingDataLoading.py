b10fb82360521432e94b57760b476261766498bf
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-19 23:06:43 -0400

    Added images and set up MultiprocessingDataLoading.py for code.

--- DataManipulation/MultiprocessingDataLoading.py
+++ DataManipulation/MultiprocessingDataLoading.py
@@ -1,3 +1,23 @@
 # Jacob Miller
 # 4/19/2021
-# Project Code: Python's Built in Multiprogramming Library DataLoading Program
+# Project Code: Python's Built in Multiprogramming Library Data Loading Program
+
+import multiprocessing as mp
+import cv2
+import os
+import cProfile
+
+num_images = len([name for name in os.listdir('./Data/OldFiles') if os.path.isfile(name)])
+num_processes = 5
+
+def worker(img):
+    print('')
+
+def doWork():
+    print('')
+
+def main():
+    cProfile.run('doWork()')
+
+if __name__ == '__main__':
+    main()
