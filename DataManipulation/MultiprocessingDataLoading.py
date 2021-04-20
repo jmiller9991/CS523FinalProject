c550dd7e6d31f041a67159316fe7caa492276d1d
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-20 01:54:29 -0400

    Built image modifications

--- DataManipulation/MultiprocessingDataLoading.py
+++ DataManipulation/MultiprocessingDataLoading.py
@@ -6,12 +6,23 @@ import multiprocessing as mp
 import cv2
 import os
 import cProfile
+import math
 
 num_images = len([name for name in os.listdir('./Data/OldFiles') if os.path.isfile(name)])
 num_processes = 5
 
 def worker(img):
-    print('')
+    modWidth = math.floor(1920/2.5)
+    modHeight = math.floor(1080/2.5)
+
+    image = cv2.imread(img)
+    newimage = cv2.resize(image, (modWidth, modHeight))
+    imagegrey = cv2.cvtColor(newimage, cv2.COLOR_BGR2GRAY)
+    blurimg = cv2.GaussianBlur(imagegrey, (3, 3), 0)
+    logimg = cv2.Laplacian(blurimg, cv2.CV_64F)
+
+    return logimg
+
 
 def doWork():
     print('')
