c6cb131f85d8e3a091f43b8aef2748a5d563d68b
jmiller9991 <jacobmiller9991@gmail.com>
2021-04-22 18:04:20 -0400

    Array still does not function properly

--- DataManipulation/MultiprocessingDataLoading.py
+++ DataManipulation/MultiprocessingDataLoading.py
@@ -25,7 +25,7 @@ def collectImages():
 
 def splitArray(array):
     final_array = []
-    count = 0
+    count = 1
     valueStop = math.floor(num_images/num_processes)
     tempArray = []
     for string in array:
@@ -33,20 +33,20 @@ def splitArray(array):
 
         if count % valueStop == 0:
             final_array.append(tempArray)
-            for i in tempArray:
-                tempArray.remove(i)
+            tempArray = []
 
         count += 1
 
     return final_array
 
 
-def worker(img):
-    image = cv2.imread(img)
-    imagegrey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurimg = cv2.GaussianBlur(imagegrey, (3, 3), 0)
-    logimg = cv2.Laplacian(blurimg, cv2.CV_64F)
-    cv2.imwrite(filename=os.path.join('.\\Data\\OldFiles', f'file{random.randint(0, 1000000000000)}'), img=logimg)
+def worker(imgarr):
+    for img in imgarr:
+        image = cv2.imread(img)
+        imagegrey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurimg = cv2.GaussianBlur(imagegrey, (3, 3), 0)
+        logimg = cv2.Laplacian(blurimg, cv2.CV_64F)
+        cv2.imwrite(filename=os.path.join('.\\Data\\OldFiles', f'file{random.randint(0, 1000000000000)}'), img=logimg)
 
 
 def doWork():
